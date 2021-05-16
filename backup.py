d668dcac5b2ea8c2bab094d3a16d7edc285fba04
nadavnave <57562882+nadavnave@users.noreply.github.com>
2021-05-16 17:36:15 +0300

    working with ftp

--- backup.py
+++ backup.py
@@ -7,6 +7,7 @@ import argparse
 import datetime
 import ftplib
 import json
+import logging
 from datetime import date
 from tqdm import tqdm
 
@@ -17,12 +18,15 @@ parser.add_argument('ftp_port',type=int, help='The port of the ftp server on the
 parser.add_argument('user',type=str, help='The user to connect to on the ftp server')
 parser.add_argument('password',type=str, help='The password to connect to on the ftp server')
 
+logging.basicConfig(filename='backup.log',level=logging.DEBUG)
+
 
 def ftp_dir_files(ftp,path):
     ftp.cwd(path)
     dir_lines = []
     ftp.retrlines('LIST',dir_lines.append)
     return [dir_line.split(' ')[-1] for dir_line in dir_lines if ftp_is_file(dir_line)]
+
 
 def ftp_is_file(dir_line):
     permisstions = dir_line.split(' ')[0]
@@ -31,9 +35,9 @@ def ftp_is_file(dir_line):
     return True
     
 
-def create_database(path):
+def create_ftp_database(ftp, path):
     df = pd.DataFrame(columns=["year","month","day","fullname"])
-    files = [f for f in os.listdir(path) if isfile(join(path,f))]
+    files = ftp_dir_files(ftp, path)
 
     for f in files:
         filedict = {}
@@ -56,47 +60,56 @@ def create_database(path):
     return df
  
 
-def delete(origin_path, keep_month):
-    df = create_database(origin_path)
+def delete(ftp, origin_path, keep_month):
+    df = create_ftp_database(ftp, origin_path)
     cur_month = date.today().month
     cur_year = date.today().year
-    df_files_to_delete = df[ ~((df['month'] >= (cur_month - keep_month)) &
-    (df['year'] == cur_year))]
-    files_to_delete = df_files_to_delete['fullname'].list()
+    df_files_to_delete = df[ ~((df['month'] >= (cur_month - keep_month)) & (df['year'] == cur_year))]
+    files_to_delete = df_files_to_delete['fullname'].tolist()
     for file in tqdm(files_to_delete):
-        os.remove(join(origin_path,file))
-        print("deleted {}".format(file))
+        ftp.delete(join(origin_path,file))
+        logging.debug("deleted {}".format(file))
 
 
-def backup(origin_path, target_path):
+def backup(ftp, origin_path, target_path):
 
-    origin_files = set([f for f in os.listdir(origin_path) if isfile(join(origin_path,f))])
-    target_files = set([f for f in os.listdir(target_path) if
-        isfile(join(target_path,f))])
+    origin_files = set(ftp_dir_files(ftp, origin_path))
+    target_files = set([f for f in os.listdir(target_path) if isfile(join(target_path,f))])
     
     not_backed_up = origin_files.difference(target_files)
     
+    ftp.cwd(origin_path)
     for file in tqdm(not_backed_up):
-        shutil.copy(join(origin_path,file),join(target_path,file))
+        with open(join(target_path,file),"wb") as fp:
+            ftp.retrbinary('RETR {}'.format(file), fp.write)
 
   
 def main():
     args = parser.parse_args()
 
     ip = args.ftp_ip
-    port = args.port
-    user = args.user
+    port = args.ftp_port
+    user = args.user 
     passwd = args.password
-    create_database(origin_path)
+
+    logfile = open("log.txt","w")
+
+    ftp = ftplib.FTP()
+    res = ftp.connect(host=ip,port=port)
+
+    res = ftp.login(user=user, passwd=passwd)
+
+    
     with open('conf.json','r') as f:
         config = json.load(f)
 
     for obj in config:
-        create_database(obj[0])
+        create_ftp_database(ftp, obj[0])
+        backup(ftp, obj[0], obj[1])
+        delete(ftp, obj[0], obj[2])
 
-    #backup(origin_path, target_path)
-    #delete(origin_path,number_month_keep)
 
 
 if __name__ == "__main__":
     main()
+
